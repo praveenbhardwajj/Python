78daaa6fad60c2bdc9c7e48e7fb2bbb4881a86f5
Praveen Bhardwaj <praveenbhardwaj@gmail.com>
2026-01-23 11:26:08 +0530

    Day 2 String and Conditional Practice

---
+++ Day2 String And Conditional Statements/HWQuestions.py
@@ -0,0 +1,23 @@
+# num1=int(input("Enetr Num1 :"))
+# num2=int(input("Enetr Num2 :"))
+# num3=int (input("Enetr Num3 :"))
+# if(num1>num2 and num1>num3):
+#     print("Greter ",num1)
+# elif(num2>num3):
+#     print("Greater ", num2)
+# else:
+#     print("Greater ", num3)
+
+
+
+num1=int(input("Enetr Num1 :"))
+
+# if(num1%2==0):
+#     print("Number is even")
+# else:
+#     print("Number is odd")
+
+if(num1%7==0):
+    print("Number is divisible by 7")
+else:
+    print("Number is not divisible by 7")
---
+++ Day2 String And Conditional Statements/PracticeQuestion.py
@@ -0,0 +1,6 @@
+# name=input("Enter your name: ")
+# print("Hello ," , name , " Your name has length of",len(name))
+
+message="hello i have 22$ aand i purches 30$ bananas and now remaing $10 i buy 5$ apples"
+
+print(message.count("$"))
