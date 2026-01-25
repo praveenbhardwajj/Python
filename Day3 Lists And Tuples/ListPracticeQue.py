7519e5c8b2d4a42fef9a2dcd7fa1ecfb1a446964
Praveen Bhardwaj <praveenbhardwaj@gmail.com>
2026-01-25 15:40:32 +0530

    old commits

--- Day3 Lists And Tuples/ListPracticeQue.py
+++ Day3 Lists And Tuples/ListPracticeQue.py
@@ -1,7 +1,10 @@
 # name1=input("Enter first favorite movie name: ")
 # name2=input("Enter second favorite movie name: ")
 # name3=input("Enter third favorite movie name: ")
-# fav_movie_list=[name1,name2,name3]
+# fav_movie_list=[]
+# fav_movie_list.append(name1)
+# fav_movie_list.append(name2)
+# fav_movie_list.append(name3)
 # print("Your favorite movies are:",fav_movie_list)
 
 # Question 2:
